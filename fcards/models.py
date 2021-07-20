156f349367864fbf8e67d54bedbfed97d48f78de
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-20 22:38:56 +0300

    adding a class card

--- fcards/models.py
+++ fcards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -18,6 +19,7 @@ class Flashcard(models.Model):
     
     name =models.CharField(max_length=200)
     title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description =models.TextField()
     category = models.ForeignKey(Category,on_delete=models.CASCADE,default='0')
     date= models.DateField()
