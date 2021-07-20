d49daa2a667ef2c6ffa863547255589519b08e1f
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-20 22:34:33 +0300

    adding a class card

--- fcards/models.py
+++ fcards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()    
+
+
+class Flashcard(models.Model):
+
+    
+    name =models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    description =models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default='0')
+    date= models.DateField()
